d88281a9890e1253c4c941d86168976a502360ec
Aman Gupta <79080069+Aman30091999@users.noreply.github.com>
2021-02-16 14:56:17 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,10 @@
+ackage vec;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+public class Vec {
+    public static void main(String[] args)throws Exception {
+       ArrayList<Integer> v=new ArrayList<>();
+       System.out.println(v.add(Integer.SIZE));
+    }
+    
+}

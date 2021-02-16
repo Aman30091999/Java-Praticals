4235b9ac5aa4ed5b934d6b17113b3e46b13fb188
Aman Gupta <79080069+Aman30091999@users.noreply.github.com>
2021-02-16 13:35:05 +0530

    Create Do-while.java

---
+++ Do-while.java
@@ -0,0 +1,10 @@
+package aq;
+public class Aq {
+    public static void main(String[] args) {
+       int x=1,y=3;
+        do{
+                    System.out.println("hard");
+    }while(x<y);
+        System.out.print("buy");
+    }
+}

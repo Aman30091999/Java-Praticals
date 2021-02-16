32b1fcb2b2fb6bccbde029c04bcc5c1f7423d71d
Aman Gupta <79080069+Aman30091999@users.noreply.github.com>
2021-02-16 13:31:51 +0530

    Add files via upload

---
+++ Stringt.java
@@ -0,0 +1,7 @@
+class Stringt {
+    public static void main(String args[]){
+        StringBuffer str= new StringBuffer("roll is:->");
+        str.append(12);
+        System.out.println(str);
+    }
+}

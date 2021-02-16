1a8c2f54bc9f67325b1ae35a3c5f7342f230ab39
Aman Gupta <79080069+Aman30091999@users.noreply.github.com>
2021-02-16 14:28:03 +0530

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,16 @@
+package n;
+import java.lang.Long;
+public class N {
+    public static void main(String[] args) {
+    //functions 
+       long a=78L;      
+       long b=68L;
+      System.out.println(Long.toBinaryString(a));      
+      System.out.println(Long.numberOfLeadingZeros(a));
+      System.out.println(Long.toBinaryString(b));
+      System.out.println(Long.numberOfTrailingZeros(b));
+
+
+    }
+    
+}

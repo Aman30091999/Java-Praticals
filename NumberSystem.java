946284dac095d9392bd0ac56c6b6533a0f156217
Aman Gupta <79080069+Aman30091999@users.noreply.github.com>
2021-02-16 14:47:12 +0530

    Create NumberSystem.java

---
+++ NumberSystem.java
@@ -0,0 +1,14 @@
+package rty;
+import java.util.Scanner;
+public class Rty {
+    public static void main(String[] args) {
+        long a=8l;
+        long b=-9l;
+        long c=0l;
+        System.out.println("a:" +Long.toBinaryString(a));
+        System.out.println("b:" +Long.toHexString(b));
+        System.out.println("c:" +Long.toOctalString(c));
+
+    }
+    
+}

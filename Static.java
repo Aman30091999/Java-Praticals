6a748e51e663bf855547bbf5460114e067bac1d5
Aman Gupta <79080069+Aman30091999@users.noreply.github.com>
2021-02-16 14:22:30 +0530

    Create Static.java

---
+++ Static.java
@@ -0,0 +1,18 @@
+package m;
+public class M{
+    public static void main(String[] args) {
+    M m= new M();
+    }
+    static int a=10;
+    static int n;
+    int b=5,c;
+    public void  M(int m){
+        System.out.println(a+" "+b+" "+c+" "+n+" "+m);
+    }{
+    b=30;
+    n=20;
+    
+}
+static {a=60;
+}
+}
